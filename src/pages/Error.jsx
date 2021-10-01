055913ebdcf320fac6f95656fb288a64b812ab5c
Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>
2021-10-01 04:57:23 -0500

    chore: improve error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,29 @@
+import PropTypes from 'prop-types'
 
-const Error = () => (
+const Error = ({ errorInfo = { errorInfo: undefined} }) => (
   <div>
-    <h1>Error</h1>
+    <p>
+      There was an error in loading this page.{' '}
+      <button
+        style={{ cursor: 'pointer', color: '#0077FF' }}
+        onClick={() => {
+          window.location.reload()
+        }}
+      >
+        Reload this page
+      </button>{' '}
+    </p>
+    <details className="error-details">
+      <summary>Click for error details</summary>
+      {errorInfo && errorInfo.componentStack.toString()}
+    </details>
   </div>
 )
+
+Error.propTypes = {
+  errorInfo: PropTypes.shape({
+    componentStack: PropTypes.object.isRequired,
+  }),
+}
 
 export default Error
