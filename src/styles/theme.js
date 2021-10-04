e2ce34efa73c139e806d6136a5b08bf01fea5729
Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>
2021-10-04 00:56:09 -0500

    chore: add breakpoints and erase font import

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,32 +2,44 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
 import { Normalize } from 'styled-normalize'
 import colors from './colors'
+import { breakpoints } from './breakpoints'
 
 const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap');
-
 :root {
-  --primary: var(${({theme}) => theme.colors.blue08}, #4513FE);
-  --secondary: var(${({theme}) => theme.colors.yellow06}, #FFCC00);
-  --black: var(${({theme}) => theme.colors.gray09}, #334A5E);
+  --primary: ${({ theme }) => theme.colors.blue08};
+  --secondary: ${({ theme }) => theme.colors.yellow06};
+  --black: ${({ theme }) => theme.colors.gray09};
   --white: white;
-  --danger: var(${({theme}) => theme.colors.gray09}, #334A5E);
+  --danger: ${({ theme }) => theme.colors.gray09};
 
   --font-family: 'Poppins', sans-serif;
+  --z-normal: 0;
+  --z-map: 10;
+  --z-map-item: 20;
+  --z-modal-overlay: 30;
+  --z-modal:40;
+
+  --max-width: 1200px;
 }
 
 body {
   font-family: var(--font-family, Poppins), sans-serif;
+  font-size: 16px;
+}
+
+h1, h2, h3, h4, h5, h6, p, span, b, strong, em, i, a {
+  color: inherit; 
+  font-family: var(--font-family);
+  -webkit-font-smoothing: auto;
+  -moz-osx-font-smoothing: auto;
+  letter-spacing: 0em;
+  overflow-wrap: break-word;
 }
 `
 
 const theme = {
   colors,
-  fontSizes: {
-    small: '1em',
-    medium: '2em',
-    large: '3em',
-  },
+  breakpoints
 }
 
 const Theme = ({ children }) => (
