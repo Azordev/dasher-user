e9b98e1ab1a2b50a4770d2f46c1099e36cfb7ed3
denisosuna <denisosuna@gmail.com>
2021-10-07 23:32:13 -0400

    feat: added JSDoc comments

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,11 +1,17 @@
 import { useMutation } from '@apollo/client'
 import { INSERT_ERRORS_MUTATION } from '../services/GraphQl'
 
+/**
+ * @callback insertErrors receive two params 
+ * @param type 
+ * @param location
+ *  to use => insertErrors({"type": type, "location": location})
+ */
+
 export function InsertErrors() {
   const [insertErrors, { loading, error, data }] = useMutation(INSERT_ERRORS_MUTATION)
   if (error) {
     console.error(error)
   }
-  // to use => insertErrors({"type": type, "location": location})
-  return { insertErrors, loading, error, data }
+ return { insertErrors, loading, error, data }
 }
