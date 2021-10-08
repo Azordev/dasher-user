f838c47fef7450ee4bcc55a5a843a67d20244a3e
Alejandro Salcido <alejandrosalciete77@gmail.com>
2021-10-08 01:12:46 -0600

    feat: basic input component added

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -1,15 +1,15 @@
-import ticket from "../assents/ticket.png";
-import warning from "../assents/warning.png";
-import arrow from "../assents/arrow-left.svg";
+import ticket from '../assents/ticket.png'
+import warning from '../assents/warning.png'
+import arrow from '../assents/arrow-left.svg'
 
 const Check = () => (
   <div>
     <div className="check-ticket">
-      <img src={arrow} alt="Arrow left"/>
-      <img src={ticket} alt="Ticket"/>
+      <img src={arrow} alt="Arrow left" />
+      <img src={ticket} alt="Ticket" />
     </div>
     <div className="pop-up">
-      <img src={warning} alt="Warning"/>
+      <img src={warning} alt="Warning" />
     </div>
   </div>
 )
