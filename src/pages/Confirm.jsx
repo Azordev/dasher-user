a13d85e964842b551c24734027ba673eef2c1cc1
Ginna Acero Russi <76055458+ginnaaceror@users.noreply.github.com>
2021-10-08 15:31:03 -0500

    Feature/#29 text component (#74)
    * feat: add images and icons
    
    * fix: delete comments, caret and react-icons library. Also add SVG icons
    
    * fix: dependency with webpack
    
    * fix: set arrow size
    
    * fix: set size gps icon
    
    * fix: add // eslint-disable-next-line
    
    * feat: add text component
    
    * perf: add text in all components with style components
    
    * perf: add all texts using style components
    
    * perf: set the text component according to the comments
    
    * fix: delete comments in Text component
    
    * perf: set prop for the colors in Text component
    
    Co-authored-by: Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>

--- src/pages/Confirm.jsx
+++ src/pages/Confirm.jsx
@@ -1,10 +1,11 @@
 import { useParams, useHistory } from "react-router-dom";
-import note from "../assents/note.png";
-import handshake from "../assents/handshake.png";
-import RatingMan from "../assents/man-calification.png"
-import arrow from "../assents/arrow-left.svg";
-import star from "../assents/star.svg";
-import close from "../assents/close-icon.svg";
+import { Text } from '../components/Text/Text';
+import note from "../assets/note.png";
+import handshake from "../assets/handshake.png";
+import RatingMan from "../assets/man-calification.png"
+import arrow from "../assets/arrow-left.svg";
+import star from "../assets/star.svg";
+import close from "../assets/close-icon.svg";
 
 
 const Confirm = () => {
@@ -19,16 +20,29 @@ const Confirm = () => {
     <div>
       <div className="confirm-header">
         <img src={arrow} alt="Arrow left"/>
+        <Text as="h1" color="secondary"medium>Hemos terminado</Text>
+        <Text small>Inserta tus datos para finalizar</Text>
         <img src={note} alt="Note icon"/>
+        <Text color="secondary" bold uppercase>Confirma recepción</Text>
+        <Text as="input" placeholder="Nombre"/> 
+        <Text as="input" placeholder="RUT"/>  
+        <Text as="input" placeholder="Celular"/>  
+        <Text as="button" bold uppercase>Confirmar</Text>
       </div>
       <div className="calification-modal">
         <img src={close} alt="Close icon"/>
+        <Text as="h1" color="primary" small>¿Qué tal tu experiencia?</Text>
+        <Text color="danger" small>La propina esta en tus manos</Text>
+        <Text color="danger">Si deseas, puedes compartirla.</Text>
+        <Text as="button" bold uppercase>Aceptar</Text>
         <img src={RatingMan} alt="Delivery man"/>
         <img src={star} alt="Stars rating"/>
       </div>
       <div className="thanks-modal">
         <img src={close} alt="Close icon"/>
         <img src={handshake} alt="Handshake"/>
+        <Text as="h1" color="primary" small>¡Gracias por confiar en nosotros!</Text>
+        <Text as="button" bold uppercase>Aceptar</Text>
       </div>
     </div>
   )
