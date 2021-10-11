7458aadd44c079ee754b7231378a9f5af5f120e3
Alejandro Salcido <80230445+AlejandroSalcido@users.noreply.github.com>
2021-10-11 09:04:15 -0600

    Add-Modal-Component-#24 (#72)
    * chore: create basic modal component structure
    
    * perf: make modal component reusable
    
    * perf: prop-types added
    
    * perf: added function to handle click
    
    * fix: overlay color and basic responsiveness fixed
    
    * fix: prop types deleted from dependencies package
    
    * fix: packages build
    
    * fix: cspell spanish
    
    * feat: improve modal code
    
    * feat: improve modal code
    
    * fix: add lock files
    
    Co-authored-by: Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -1,34 +1,54 @@
+import { useState } from 'react'
+import { Modal } from '../components'
 
-import { Text } from '../components/Text/Text';
-import ticket from "../assets/ticket.png";
-import warning from "../assets/warning.png";
-import arrow from "../assets/arrow-left.svg";
+import { Text } from '../components/Text/Text'
+import ticket from '../assets/ticket.png'
+import warning from '../assets/warning.png'
+import arrow from '../assets/arrow-left.svg'
+import { useHistory } from 'react-router'
 
-const Check = () => (
-  <div>
-    <div className="header-ticket">
-      <img src={arrow} alt="Arrow left"/>
-      <Text as="h1" color="secondary" medium>Hola!</Text>
-      <Text small>Ingresa tu número de boleta</Text>
-      <img src={ticket} alt="Ticket"/>
+const Check = () => {
+  const [isModalOpen, changeIsModalOpen] = useState(true)
+  const history = useHistory();
+
+  const toDelivery = (e) => {
+    e.preventDefault()
+    history.push('/delivery/lol')
+  }
+
+  return (
+    <div>
+      <div className="header-ticket">
+        <img src={arrow} alt="Arrow left" />
+        <Text as="h1" color="secondary" medium>
+          Hola!
+        </Text>
+        <Text small>Ingresa tu número de boleta</Text>
+        <img src={ticket} alt="Ticket" />
+      </div>
+      <div className="check-ticket">
+        <Text color="secondary" bold uppercase>
+          Confirmar n° boleta o pedido
+        </Text>
+        <Text as="input" placeholder="Ingresa aquí..." />
+        <Text as="button" bold uppercase onClick={e=>toDelivery(e)}>
+          Confirmar
+        </Text>
+      </div>
+      <Modal isOpen={isModalOpen} handleClick={() => changeIsModalOpen(false)} actionText="Entendido">
+        <img src={warning} alt="Warning" />
+        <Text as="h1" color="primary" small>
+          Antes de recibir...
+        </Text>
+        <Text color="danger">No olvides confirmar la recepción de este envío al final de este proceso ingresando:</Text>
+        <Text as="ul" color="primary" bold>
+          <li>Nombre</li>
+          <li>RUT</li>
+          <li>Celular</li>
+        </Text>
+      </Modal>
     </div>
-    <div className="check-ticket">
-      <Text color="secondary" bold uppercase>Confirmar n° boleta o pedido</Text>
-      <Text as="input" placeholder="Ingresa aquí..."/>
-      <Text as="button" bold uppercase>Confirmar</Text>
-    </div>
-    <div className="pop-up">
-      <img src={warning} alt="Warning"/>
-      <Text as="h1" color="primary" small>Antes de recibir...</Text>
-      <Text color="danger">No olvides confirmar la recepción<br/>de este envío al final de este<br/>proceso ingresando:</Text>
-      <Text as="ul" color="primary" bold>
-        <li>Nombre</li>
-        <li>RUT</li>
-        <li>Celular</li>
-      </Text>
-      <Text as="button" bold uppercase>Entendido</Text>
-    </div>
-  </div>
-)
+  )
+}
 
 export default Check
