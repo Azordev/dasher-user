2179091e5596747553a38682976509e59dedbe7c
Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>
2021-10-12 00:19:23 -0500

    feat: basic responsiveness added

--- src/pages/Check.jsx
+++ src/pages/Check.jsx
@@ -60,6 +60,9 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background: white;
+  @media (min-width: 960px) {
+    flex-direction: row;
+  }
 `
 
 const Header = styled.div`
@@ -67,23 +70,46 @@ const Header = styled.div`
   flex-direction: column;
   background: blue;
   border-radius: 0 0 25px 25px;
+  @media (min-width: 960px) {
+    width: 50%;
+    border-radius: 0000;
+    justify-content: start;
+    overflow: hidden;
+  }
 `
 const BackButton = styled.img`
   width: 30px;
   margin: 15px 8px;
+  @media (min-width: 960px) {
+    top: 30px;
+    left: 50px;
+    position: fixed;
+    width: 45px;
+  }
 `
 
 const TicketImage = styled.img`
   width: 140px;
   align-self: flex-start;
   margin: 0 0 -13px 15px;
+  @media (min-width: 960px) {
+    width: 40vh;
+    bottom: -38px;
+    align-self: flex-end;
+    position: fixed;
+    margin-left: -50px;
+  }
 `
-
 const MainSection = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  margin-top: -100px;
+  height: 100%;
+  @media (min-width: 960px) {
+    background: yellow;
+    width: 50%;
+    height: 100vh;
+    justify-content: center;
+  }
 `
