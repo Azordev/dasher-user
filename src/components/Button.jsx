d157eb1790f885761e7414f4330da2c59251ad45
Israel Antonio Rosales Laguan <irosales@fullstacklabs.co>
2021-10-11 22:09:06 -0500

    feat: basic button component added

---
+++ src/components/Button.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Button = ({ children }) => {
+  return (
+    <div>
+      <ButtonWrapper>
+        <MainButton>{children}</MainButton>
+      </ButtonWrapper>
+    </div>
+  )
+}
+
+Button.propTypes = {
+  children: PropTypes.any,
+}
+
+export default Button
+
+const ButtonWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 25px 0;
+`
+const MainButton = styled.button`
+  width: 65%;
+  border-radius: 20px;
+  height: 40px;
+  border: none;
+  box-shadow: 2px 5px 6px rgb(0 0 0 / 0.3);
+  padding: 0 25px;
+  background: blue;
+  color: white;
+  font-weight: bold;
+`
