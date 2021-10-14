1376dab1073b59aa84608191a20fcd624da148c9
ginnaaceror <76055458+ginnaaceror@users.noreply.github.com>
2021-10-14 18:46:16 -0500

    feat: add page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,21 @@
+import notFound from '../assets/not-found.svg'
+import styled from 'styled-components'
+import { Text } from '../components'
+
+const NotFoundImg = styled.img`
+  display:block;
+  margin:auto;
+  width: 70%;
+  height: 70%;
+`
+
 const NotFound = () => (
   <div>
-    <h1>404</h1>
+    <NotFoundImg src={notFound} alt="Image Not-found" />
+    <Text color="primary" bold center large>
+      Oops!<br/>
+      <Text color="primary" uppercase>Page not found</Text>
+    </Text>
   </div>
 )
 
