2f122aafb471992a922ba38ddb891f7e7bcf013b
ginnaaceror <76055458+ginnaaceror@users.noreply.github.com>
2021-10-15 06:00:46 -0500

    fix: center the content in mobile

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,21 +2,28 @@ import  NotFound from '../assets/not-found.svg'
 import styled from 'styled-components'
 import { Text } from '../components'
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  height: 100vh;
+`
+
 const NotFoundImg = styled.img`
-  display:block;
-  margin:auto;
-  width: 70%;
-  height: 70%;
+  width: 80%;
+  padding: 0%;
 `
 
 const ErrorNotFound = () => (
-  <div>
-    <NotFoundImg src={NotFound} alt="Image Not-found" />
+  <Container>
+    <NotFoundImg src={NotFound} alt="Image Not-found"/>
     <Text color="primary" bold center large>
       Oops!<br/>
       <Text color="primary" uppercase>Page not found</Text>
     </Text>
-  </div>
+  </Container>
 )
 
 export default ErrorNotFound;
