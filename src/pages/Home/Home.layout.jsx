75faa9c65a4dfa7acc6a4d110ab776baefe15aad
Kembert Nieves <kembert.nieves@kavak.com>
2021-11-09 11:44:55 -0400

    fix: improve web styles into home page

--- src/pages/Home/Home.layout.jsx
+++ src/pages/Home/Home.layout.jsx
@@ -1,8 +1,9 @@
 import deliveryMan from '../../assets/delivery-man.png'
 import logo from '../../assets/logo.svg'
+import Colors from '../../styles/colors'
 import PropTypes from 'prop-types'
 
-import { Button, Header, Logo, ManImage, Text } from './Home.styled'
+import { Button, Header, Logo, ManImage, DisplayText, SubtitleText, HalfSection, TextContainer } from './Home.styled'
 
 /**
  * Home Layout
@@ -16,13 +17,24 @@ import { Button, Header, Logo, ManImage, Text } from './Home.styled'
  */
 const Home = ({ title, subtitle, buttonText }) => (
   <Header>
-    <Logo src={logo} alt="logo" />
-    <Text as="h1" color="secondary" large>
-      {title}
-    </Text>
-    <Text small>{subtitle}</Text>
-    <ManImage src={deliveryMan} alt="Delivery man" />
-    <Button to="/check">{buttonText}</Button>
+    <HalfSection bg={Colors.yellow06}>
+      {/**/}
+      <Logo src={logo} alt="logo" />
+      <ManImage style={{ display: 'nones' }} src={deliveryMan} alt="Delivery man" />
+      {/**/}
+    </HalfSection>
+
+    <HalfSection bg={Colors.blue08} fxJtyCt="center" fxAlIt="center">
+      <TextContainer>
+        {/**/}
+        <DisplayText as="h1" color="secondary" large>
+          {title}
+        </DisplayText>
+        <SubtitleText small>{subtitle}</SubtitleText>
+        <Button to="/check">{buttonText}</Button>
+        {/**/}
+      </TextContainer>
+    </HalfSection>
   </Header>
 )
 
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -3,38 +3,84 @@ import { Link } from 'react-router-dom'
 import { Text as OriginalText } from '../../components'
 import text from '../../styles/text'
 
-export const Text = styled(OriginalText)`
-  ${[text.textCenter]}
-  ${props => props.title && text['7xl']}
-`
-
 export const Header = styled.div`
-  position: fixed;
+  display: flex;
+  flex-direction: column-reverse;
   width: 100%;
-  height: 55%;
-  background-color: var(--primary);
-  display: block;
-  margin: 0 auto;
+  height: 100%;
+  max-width: 100vw;
+  max-height: 100vh;
 
   @media (min-width: 960px) {
-    position: fixed;
-    margin-left: 50%;
-    width: 50%;
-    height: 100%;
-    background-color: var(--primary);
+    flex-direction: row;
+  }
+`
+export const HalfSection = styled.div`
+  ${props => `background-color: ${props.bg};`}
+  ${props => `justify-content: ${props.fxJtyCt};`}
+  ${props => `align-items: ${props.fxAlIt};`}
+
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 960px) {
+    width: 100%;
+    min-height: 100vh;
+  }
+`
+export const Logo = styled.img`
+  display: none;
+  max-width: 30%;
+
+  @media (min-width: 960px) {
+    display: block;
+    margin-top: 8%;
+    margin-left: 8%;
   }
 `
 export const ManImage = styled.img`
   display: block;
-  margin: 0 auto;
-  height: 65vh;
+
+  width: 70%;
 
   @media (min-width: 960px) {
-    margin-left: -90%;
-    margin-top: -23%;
+    align-self: center;
+    margin-top: 12%;
+    height: 68vh;
+  }
+`
+export const TextContainer = styled.div`
+  max-width: 100%;
+
+  @media (min-width: 960px) {
+    max-width: 70%;
   }
 `
 
+export const Text = styled(OriginalText)`
+  ${[text.textCenter]}
+  ${props => props.title && text['7xl']}
+`
+export const DisplayText = styled(Text)`
+  font-size: 3rem;
+  text-align: left;
+  margin: 3rem 0 5px -5px;
+
+  @media (min-width: 960px) {
+    font-size: 6.5rem;
+    text-align: left;
+    margin: 10px 0 30px -5px;
+  }
+`
+
+export const SubtitleText = styled(Text)`
+  margin-top: 3px;
+  font-size: 1rem;
+
+  @media (min-width: 960px) {
+    margin: 15px 0 15px 0;
+    font-size: 2rem;
+  }
+`
 export const Button = styled(Link)`
   background: var(--primary);
   border-radius: 20px;
@@ -42,7 +88,7 @@ export const Button = styled(Link)`
   color: var(--white);
   height: 100%;
   max-height: 40px;
-  width: 30%;
+  width: 100%;
   align-items: center;
   justify-content: center;
   display: block;
@@ -53,18 +99,12 @@ export const Button = styled(Link)`
   line-height: 40px;
 
   @media (min-width: 960px) {
-    margin-top: -35%;
     background-color: var(--secondary);
-    width: 45%;
-  }
-`
-
-export const Logo = styled.img`
-  display: none;
-
-  @media (min-width: 960px) {
-    margin-left: -94%;
-    display: block;
-    margin-top: 10%;
+    height: 50%;
+    padding: 1rem 0 3rem 0;
+    font-weight: bold;
+    font-size: 1.5rem;
+    border-radius: 2rem;
+    letter-spacing: 2px;
   }
 `
